38e6d31584b155d501d12047784386f73d6682f0
AhgamaSundar <Ahgamasundar40@gmail.com>
2024-05-31 16:33:41 +0530

    first c++

---
+++ helloworld.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+struct array{
+  int arr[20];
+  int len;
+  int size;
+
+};
+void display(struct array ars){
+    int i;
+    printf("display\n");
+  for ( i=0;i<ars.len;i++){
+    printf("%d\n",ars.arr[i]);
+
+  }
+
+}
+void add(struct array *ars,int n){
+  ars->arr[ars->len]=n;
+  ars->len++;
+}
+void insert(struct array *ars,int n,int a){
+  for (int i=ars->len;i>n;i--){
+    
+    ars->arr[i]=ars->arr[i-1];
+    
+  }
+  ars->arr[n]=a;
+
+
+}
+void del(struct array *ar,int n){
+  int i;
+  int a;
+  a=ar->arr[n];
+  if (n>=0 && n<=ar->len){
+  for ( i=n;i<ar->len;i++){
+    ar->arr[i]=ar->arr[i+1];
+  }
+  ar->len--;
+  }
+  printf("Deleted %d \n",a);
+}
+void swap(int *i,int *y){
+  int k;
+  k=*y;
+  *y=*i;
+  *i=k;
+
+  
+
+}
+int linsr(struct array *ar,int n){
+  int i;
+  for(i=0;i<ar->len;i++){
+    if(ar->arr[i]==n){
+    swap(&ar->arr[i],&ar->arr[0]);
+    return i;
+    
+  }
+  
+}
+return -1;
+}
+
+int main(){
+  struct array ars={{1,2,3,4,5},5,10};
+  add(&ars,20);
+  insert(&ars,0,70);
+  display(ars);
+  del(&ars,0);
+  display(ars);
+  printf("search  %d\n",linsr(&ars,5));
+  printf("search  %d\n",linsr(&ars,5));
+  printf("search %d\n",linsr(&ars,5));
+  display(ars);
+
+}
