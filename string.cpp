b61af4d7c6503351424dfba3d8af415dc03fd5f8
AhgamaSundar <ahgamasundar40@gmail.com>
2024-06-26 19:37:42 +0530

    added the same

--- string.cpp
+++ string.cpp
@@ -4,6 +4,9 @@ int main(){
     char i;
     char str[]={'a','b','c','\0'};
     i='a';// can only be given one character while using "char" type 
-    printf("%c",str[3]);
+    printf("%s",str);
+    scanf("%s",str);
+    printf("%s",str);
+    
 
 }
