49c206d17b86031f42aab390c178198ef6ea0e49
AhgamaSundar <ahgamasundar40@gmail.com>
2024-07-02 17:20:01 +0530

    Added a way to find palindrome

--- string.cpp
+++ string.cpp
@@ -13,6 +13,7 @@ void valid(char name[]){
 
 
 }
+
 void reverse(char string[]){ //we can also use a tem variable to avoid using another array
     int i;
     for (i=0;string[i]!='\0';i++){
@@ -22,12 +23,29 @@ void reverse(char string[]){ //we can also use a tem variable to avoid using ano
     i=i-1;
     for(int j=0;i>=0;j++,i--){
         str[j]=string[i];
+        
     
     }
-    printf("%s",str);
-}
-int main(){
+    //checking if palindrome
+    bool pali =true;
+    for (int j=0;str[j]!='\0'&&string[j]!='\0' ;j++){
+        if (str[j]!=string[j]){
+            pali =false;
+            break;
+        }
+    }
     
+    printf("\n %s",str);
+    if(pali){
+        printf(" \n %s is a palindrome ",string);
+    }
+    else{
+        printf(" Not a palindrome");
+    }
+}
+//for comparing two strings with the exception of case convert the strirng a lower or a upper one
+int main(){
+    char a[]="meem";
     char str[]={'h','o','w',' ','a','r','e',' ','y','o','u','1','#','f','i','n','e','$','\0'};
     // can only be given one character while using "char" type 
    
@@ -52,7 +70,7 @@ int main(){
 
     }
     printf("%s",str);
-     reverse(str);
+     reverse(a);
 
      
 }
